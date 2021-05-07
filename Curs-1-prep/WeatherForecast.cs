567881f8990973ab3afbd2754ccaf564a1fc6bfc
Ionescu Vlad <ionescu.vlad1@gmail.com>
2021-05-07 20:29:37 +0300

    initial commit

---
+++ Curs-1-prep/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Curs_1_prep
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
